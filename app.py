94b1f6ef6668a250c845c6169d466f45f352e612
matt12sang <matthieu.mercier@ehl.ch>
2025-04-07 22:51:01 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("model.pkl")
+le_jour = joblib.load("le_jour.pkl")
+le_meteo = joblib.load("le_meteo.pkl")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    jour = le_jour.transform([data["jour"]])[0]
+    meteo = le_meteo.transform([data["meteo"]])[0]
+    heure = int(data["heure"])
+    evenement = int(data["evenement"])
+    X = [[jour, heure, meteo, evenement]]
+    prediction = model.predict(X)[0]
+    return jsonify({"prediction": round(prediction)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
